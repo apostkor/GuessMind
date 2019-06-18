729c970d9102b4e4e6cfb9dda148baedb5ac56c6
어포스트 (apost) <45118109+apostkor@users.noreply.github.com>
2019-06-19 08:51:52 +0900

    test

--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
-app.use(express.static(join(__dirname, "static")));
 app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
+
 app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () => console.log(`!!! Server Running: http://localhost:${PORT}  !!!`);
@@ -17,4 +18,8 @@ const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
 
-io.on("connection", () => console.log("Somebody Connected"));
+
+io.on("connection", socket => {
+  socket.on("helloGuys", () => console.log("the client said hello"));
+});
+
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,6 @@
-Hello;
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+socket.on("hello", () => console.log("Somebody joined"));
+
+socket.emit("helloGuys");
